ca10e7f67603a1b6d75c36493bd92139d65acc41
Marta Grana <marta.grana.fdez@gmail.com>
2024-07-16 12:56:59 +0200

    4.80 cross-component comunication with Outputs

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,6 +9,13 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number,
+    duration: number,
+    expectedReturn: number,
+    annualInvestment: number
+  }>();
+
   enteredInitialInvestment = '0';
   enterdAnnualInvestment = '0';
   enteredExpectedReturn = '5';
@@ -16,11 +23,12 @@ export class UserInputComponent {
 
   onSubmit() {
 
-    console.log('SUBMITTED!');
-    console.log(this.enteredInitialInvestment);
-    console.log(this.enterdAnnualInvestment);
-    console.log(this.enteredExpectedReturn);
-    console.log(this.enteredDuration);
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+      annualInvestment: +this.enterdAnnualInvestment
+    });
   }
 
 }
