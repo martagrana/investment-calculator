09a7d6ed4066473eb1ac4884d0c543d49dcf17b8
Marta Grana <marta.grana.fdez@gmail.com>
2024-07-17 11:00:02 +0200

    4.86 using service for cross component comunication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
-import { InvestmentInput } from './investment-input.model';
+
 import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
@@ -11,37 +11,7 @@ import { InvestmentResultsComponent } from './investment-results/investment-resu
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  resultsData = signal<{
-    year: number;
-    interest: number;
-    valueEndOfYear: number;
-    annualInvestment: number;
-    totalInterest: number;
-    totalAmountInvested: number
-  }[] | undefined>(undefined);
 
-  onCalculateInvestmentResults(data: InvestmentInput
-  ) {
-    const { initialInvestment, duration, expectedReturn, annualInvestment } = data;
-    const annualData = [];
-    let investmentValue = initialInvestment;
 
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
 
-    this.resultsData.set(annualData);
-  }
 }
